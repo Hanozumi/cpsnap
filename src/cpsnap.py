adaa1b9483ce857e5b6bb6881a627cf0009ed91b
Oliver Pauls <mail@oliverpauls.de>
2025-03-07 20:54:36 +0100

    Header Init

---
+++ src/cpsnap.py
@@ -0,0 +1,10 @@
+####################################################
+#                                                  #
+#                    - cpsnap -                    #
+#                                                  #
+#      Simple backup and snapshot CLI utility      #
+#            for local and remote usage            #
+#                                                  #
+#            Copyright © 2025 Hanozumi             #
+#                                                  #
+####################################################
