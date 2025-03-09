53ff5a708c6adfe5d494159f1fe5f6908763fd63
Oliver Pauls <mail@oliverpauls.de>
2025-03-09 14:29:39 +0100

    config parser; added exceptions and moved to class => easier access

--- src/cpsnap_config.py
+++ src/cpsnap_config.py
@@ -7,40 +7,55 @@
 
 import csv
 
-source_paths = []
-backup_path = ''
-ssh_path = ''
-retains = []
+class Config:
+	'''Config class for managing .conf files.
 
-def prepare_config(config_path: str, c_char: str, delimiter: str) -> tuple[list, str, str, list]:
-	'''Prepares a given config file and returns usable information on configuration.
-
-	Args:
-		config_path (str): Path to config file
-		c_char (str): Comment char of config file
-		delimiter (str): Delimiter/Split char of config file
-
-	Returns:
-		out (tuple): tuple containing
-		- source_paths (list): List of source paths
-		- backup_path (str): Single backup path (remote, if ssh_path set)
-		- ssh_path (str) | None: Path to remote ssh server
-		- retains (list): List of retain configurations
+	Attributes:
+		source_paths (list): List of source paths.
+		backup_path (str): Single backup path (remote, if ssh_path set).
+		ssh_path (str) | None: Path to remote ssh server.
+		retains (list): List of retain configurations.
 	'''
 
-	with open(config_path) as conf:
-		conf_raw = []
-		for row in conf:
-			raw = row.split(c_char)[0].strip()
-			if raw != '': conf_raw.append(raw)
+	def __init__(self, config_path: str, c_char: str, delimiter: str):
+		'''Prepares a given config file and returns usable information as Config object.
 
-		for line in csv.reader(conf_raw, delimiter=delimiter):
-			filtered = list(filter(lambda x: x.strip(), line))
-			match filtered[0]:
-				case 'source': 	source_paths.append(filtered[1])
-				case 'backup': 	backup_path = filtered[1]
-				case 'ssh': 	ssh_path = filtered[1]
-				# case 'retain'
-				case _: 		print(f'Unknown configuration option "{filtered[0]}"')
+		Args:
+			config_path (str): Path to config file.
+			c_char (str): Comment char of config file.
+			delimiter (str): Delimiter/Split char of config file.
 
-	return source_paths, backup_path, ssh_path, retains
+		Returns:
+			out (Config): Config object including
+			- source_paths (list): List of source paths.
+			- backup_path (str): Single backup path (remote, if ssh_path set).
+			- ssh_path (str) | None: Path to remote ssh server.
+			- retains (list): List of retain configurations.
+
+		Raises:
+			ValueError: An invalid configuration options has been set or a configuration option is missing.
+		'''
+
+		self.source_paths = []
+		self.backup_path = ''
+		self.ssh_path = ''
+		self.retains = []
+
+		with open(config_path) as conf:
+			conf_raw = []
+			for row in conf:
+				raw = row.split(c_char)[0].strip()
+				if raw != '': conf_raw.append(raw)
+
+			for line in csv.reader(conf_raw, delimiter=delimiter):
+				filtered = list(filter(lambda x: x.strip(), line))
+				match filtered[0]:
+					case 'source': 	self.source_paths.append(filtered[1])
+					case 'backup': 	self.backup_path = filtered[1]
+					case 'ssh': 	self.ssh_path = filtered[1]
+					case 'retain':	self.retains.append(filtered[1:])
+					case _: 		print(f'Unknown configuration option "{filtered[0]}"')
+
+		if len(self.source_paths) <= 0: raise ValueError("At least one source path needs to be set.")
+		if len(self.backup_path)  <= 0: raise ValueError("A backup path needs to be set.")
+		if len(self.retains)	  <= 0: raise ValueError("At least one retain option needs to be set.")
