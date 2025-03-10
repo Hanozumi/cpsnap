75d8788b521584a4dc7f74135a8235afda812d6b
Oliver Pauls <mail@oliverpauls.de>
2025-03-10 21:01:21 +0100

    name funcs; prepared name function module

---
+++ src/cpsnap_name_funcs.py
@@ -0,0 +1,15 @@
+'''
+	cpsnap_name_funcs.py
+	naming function definitions for cpsnap backups
+
+	Copyright © 2025 Hanozumi
+'''
+
+def date():
+	return
+
+def datetime():
+	return
+
+def weekday():
+	return
